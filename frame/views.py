8f96fbfa2e146ffa197c847567d0bbfa2854856b
Rushil Umaretiya <rushilwiz@gmail.com>
2021-09-28 02:34:33 -0400

    feat: finished

--- frame/views.py
+++ frame/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.conf import settings
+from allauth.socialaccount.models import SocialToken
 
 from .models import Profile
 
 from urllib.request import urlretrieve
 from django.core.files import File
+
+import requests
+import facebook
 
 # Create your views here.
 
@@ -17,5 +21,7 @@ def redirect(request):
         profile.save()
         req = urlretrieve(profile.user.socialaccount_set.first().get_avatar_url())
         profile.image.save("image.jpg", File(open(req[0], 'rb')))
-
+    else:
+        profile = Profile.objects.get(user=request.user)
+ 
     return render(request, 'frame/redirect.html', context={'data': profile})
